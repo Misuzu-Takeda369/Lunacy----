ef8749d802457744d9fe8d19e15e389607311992
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-11-27 13:01:48 +0900

    aaa

--- 2023_10daysjam/Enemy/NHEnemy.cpp
+++ 2023_10daysjam/Enemy/NHEnemy.cpp
@@ -1,5 +1,5 @@
 ﻿#include"NHEnemy.h"
-#include "Animation/BladeSlimeAnim.h"
+
 
 void NHEnemy::Initialize(Vector2 pos, Vector2 speed, float radius)
 {
@@ -21,8 +21,8 @@ void NHEnemy::Initialize(Vector2 pos, Vector2 speed, float radius)
 	moveEnemy_ = { 0.0f,0.0f };
 
 	////アニメーション初期化
-	//animation_ = new BladeSlimeAnim;
-	//animation_->Initialize();
+	/*animation_ = new BladeSlimeAnim;
+	animation_->Initialize();*/
 
 
 	
@@ -39,23 +39,23 @@ void NHEnemy::Update()
 	NaturalDeath();
 
 
-	////アニメーションUpdate
-	//animation_->Update(charaBase_.pos_);
-	//animation_->SetDirection(_right);
-	//animation_->SetMaindState(Normal);
+	//アニメーションUpdate
+	/*animation_->Update(charaBase_.pos_);
+	animation_->SetDirection(_right);
+	animation_->SetMaindState(Normal);*/
 
-
-#ifdef _DEBUG
-#pragma region ImGui関連
-
-	ImGui::Begin("Enemy");
-	ImGui::Text("EnemyPos %f.%f\n", charaBase_.pos_.x, charaBase_.pos_.y);
-	ImGui::Text("moveEnemy%f.%f\n",  moveEnemy_.x, moveEnemy_.y);
-
-	ImGui::End();
-
-#pragma endregion
-#endif // DEBUG
+//
+//#ifdef _DEBUG
+//#pragma region ImGui関連
+//
+//	ImGui::Begin("Enemy");
+//	ImGui::Text("EnemyPos %f.%f\n", charaBase_.pos_.x, charaBase_.pos_.y);
+//	ImGui::Text("moveEnemy%f.%f\n",  moveEnemy_.x, moveEnemy_.y);
+//
+//	ImGui::End();
+//
+//#pragma endregion
+//#endif // DEBUG
 
 
 }
